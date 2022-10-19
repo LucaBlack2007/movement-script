de0e6fd7d4f0a5b4041d5f1d2dcbda0c6fdd010d
lucablack2007 <68418123+LucaBlack2007@users.noreply.github.com>
2022-10-19 09:52:31 -0700

    Update Movement.cs

--- Movement.cs
+++ Movement.cs
@@ -4,24 +4,51 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
-    Vector3 vec = new Vector3(0.01f, 0f, 0f);
+    // enemy ai cube
+    GameObject cube;
+    bool cubeMoving;
+
+    // temporaries to track speed and reset
     Vector3 temp;
     Vector3 temp2;
 
-    bool moving = false;
+    //bool moving = false;
+    bool inSession;
 
+    // cooldowns
     float lastRecorded = 0f;
     float boostTime = 0f;
     float gameModeCooldown = 0f;
-    
+    float speedDevCd = 0f;
+
+    float speed;
     string GameMode;
+    Vector3 vec;
+
+    // list to track locations 
+    ArrayList locations;
 
     void Start() {
 
-        // BOUNCE or TELEPORT
+        cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.position = new Vector3(0, 0.5f, 0);
+
+        // BOUNCE or TELEPORT or DIE
         GameMode = "TELEPORT";
         // GameMode is the way the object collides
 
+        inSession = true;
+        // starts the game
+
+        speed = 0.01f;
+        // speed variable at which user travels at
+
+        vec = new Vector3(speed, 0f, 0f);
+        // actual speed vector user travels at
+
+        locations = new ArrayList();
+        cubeMoving = false;
+        // ai code initiation
     }
 
 
@@ -32,13 +59,40 @@ public class Movement : MonoBehaviour {
     // Update is called once per frame
     void Update() {  
         
+        // some logic to make sure you can edit speed before you change direction
+        if (vec.x > 0) 
+            vec = new Vector3(speed, 0f, 0f);
+        else if (vec.x < 0)
+            vec = new Vector3(-speed, 0f, 0f);
+        else if (vec.y > 0)
+            vec = new Vector3(0f, speed, 0f);
+        else if (vec.y < 0)
+            vec = new Vector3(0f, -speed, 0f);
+
         transform.Translate(vec);
+        //locations.Add(transform.position);
+        
+        if (!(transform.position.x <= -9.5 || transform.position.x >= 9.5 || transform.position.y <= -9.5 || transform.position.y >= 9.5) && inSession) {
+            if (Input.GetKey(KeyCode.A)) vec = new Vector3(-speed, 0f, 0f);    
+            if (Input.GetKey(KeyCode.D)) vec = new Vector3(speed, 0f, 0f);  
+            if (Input.GetKey(KeyCode.S)) vec = new Vector3(0.0f, -speed, 0.0f);  
+            if (Input.GetKey(KeyCode.W)) vec = new Vector3(0.0f, speed, 0.0f);  
+            
+            // speed modification dev tool
+            if ((Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow)) && Time.time - speedDevCd > 0.2) {
+                if (Input.GetKey(KeyCode.UpArrow)) speed+=0.01f;
+                if (Input.GetKey(KeyCode.DownArrow)) speed-=0.01f;
 
-        if (Input.GetKey(KeyCode.A)) vec = new Vector3(-0.01f, 0f, 0f);    
-        if (Input.GetKey(KeyCode.D)) vec = new Vector3(0.01f, 0f, 0f);  
-        if (Input.GetKey(KeyCode.S)) vec = new Vector3(0.0f, -0.01f, 0.0f);  
-        if (Input.GetKey(KeyCode.W)) vec = new Vector3(0.0f, 0.01f, 0.0f);  
+                print("Speed raised to: " + speed + " m/s");
 
+                speedDevCd = Time.time;
+            }
+            if (Input.GetKey(KeyCode.B)) {
+                cubeMoving = !cubeMoving;
+                print(cubeMoving ? "AI following you now!" : "AI stopped following you!");
+            }
+            
+        }
         /* STOP 
         if (Input.GetKey(KeyCode.Space) && (Time.time - lastRecorded) > 0.5) {
             if (moving) {// moving / not mvoing change with space bar TOGGLE
@@ -57,7 +111,7 @@ public class Movement : MonoBehaviour {
         
         // TOGGLE GAMEMODE
         if (Input.GetKey(KeyCode.G) && (Time.time - gameModeCooldown) > 1) {
-            GameMode = (GameMode == "TELEPORT") ? "BOUNCE" : "TELEPORT";
+            GameMode = (GameMode == "TELEPORT") ? "BOUNCE" : ((GameMode == "BOUNCE") ? "DIE" : "TELEPORT");
             print("GameMode set to " + GameMode);
             gameModeCooldown = Time.time;
         }
@@ -65,14 +119,21 @@ public class Movement : MonoBehaviour {
 
         // BOOST
         if (Input.GetKey(KeyCode.Space) && (Time.time - lastRecorded) > 1) {
-            
-            temp2 = vec;
-            vec = new Vector3(vec.x*3, vec.y*3, vec.z*3);
-            //print("boost STARTED");
-            boostTime = Time.time; // 1
-            lastRecorded = Time.time;
-
+            if (GameMode == "DIE") {
+                GameMode = "TELEPORT";
+                speed = 0.01f;
+                inSession = true;
+                vec = new Vector3(speed, 0f, 0f);
+                print("Game restarted, GameMode reverted to TELEPORT.");
+            } else {
+                temp2 = vec;
+                vec = new Vector3(vec.x*3, vec.y*3, vec.z*3);
+                //print("boost STARTED");
+                boostTime = Time.time; // 1
+                lastRecorded = Time.time;
+            }
         }
+
 
         if (Time.time - boostTime > 0.2 && boostTime > 0) { 
             vec = temp2;
@@ -94,11 +155,41 @@ public class Movement : MonoBehaviour {
                     transform.position = new Vector3(transform.position.x,(transform.position.y*-1)-0.05f,0);
                 if (transform.position.y >= 9.5)
                     transform.position = new Vector3(transform.position.x,(transform.position.y*-1)+0.05f,0);
+            } else if (GameMode == "DIE") { 
+                transform.position = new Vector3(0f,0f,0f);
+                speed = 0f;
+                inSession = false;
+                print("gg");
             }
+        }
+        
+        if (cubeMoving) {
+            /*
+            check location of player
+            check direction/velocity of player
+
+            */ 
+            Vector3 aiVec = vec;
+            float xDiff = 0f;
+            float yDiff = 0f;
+
+            if (transform.position.x != 0) 
+                xDiff = transform.position.x - cube.transform.position.x;
+            if (transform.position.y != 0)
+                yDiff = transform.position.y - cube.transform.position.y;
+
+            if (xDiff < 0) xDiff = -speed;
+            if (xDiff > 0) xDiff = speed;
+
+            if (yDiff < 0) yDiff = -speed;
+            if (yDiff > 0) yDiff = speed;
+
+            aiVec = new Vector3(xDiff, yDiff, 0f);
+
+            cube.transform.Translate(aiVec);
 
 
         }
-        
 
         /*
 
